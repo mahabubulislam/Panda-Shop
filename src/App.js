394efcdffe63727fb627003380a1835dc5cc6078
Mahabubul Islam <mahabub2k1@gmail.com>
2022-03-20 12:37:42 +0600

    search box added

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Menu from './Components/Menubar/Menu';
 import { useState } from 'react';
+import Search from './Components/Search/Search';
 
 function App() {
   return (
     <div className="App">
       <Menu></Menu>
+      <Search></Search>
     </div>
   );
 }
---
+++ src/Components/Search/Search.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Search = () => {
+    return (
+        <div>
+            <h3>3 days guaranteed delivary all overs country </h3>
+            <div className='container input-group mb-3 w-75'>
+                <input type="text" className='form-control' />
+                <button className='btn btn-outline-danger'>Search</button>
+            </div>
+        </div>
+    );
+};
+
+export default Search;
