08ede33603bb065ad058d8ffab0e7712495c2a28
Mahabubul Islam <mahabub2k1@gmail.com>
2022-03-20 12:25:08 +0600

    menu added

---
+++ src/Components/Menubar/Menu.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Menu.css'
+const Menu = () => {
+    return (
+        <div className='container d-flex justify-content-between bg-light'>
+            <h4 className='m-3 p-2'>Panda Shop</h4>
+            <nav className='m-3 p-2 d-flex'>
+                <li><a href="Home.js">Home</a></li>
+                <li><a href="https">Products</a></li>
+                <li><a href="https">Cart</a></li>
+                <li><a href="https">Login</a></li>
+            </nav>
+        </div>
+    );
+};
+
+export default Menu;
